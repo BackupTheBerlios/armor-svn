8f6e2e4a627ffd0709fcc51af06ba072e4ff04c5
wiecki <wiecki@d11de2a6-4b3f-0410-8375-bf80547001dc>
2008-01-16 23:14:26 +0000

    Compile updated

--- orange/lib/vlfeat/python/compile.sh
+++ orange/lib/vlfeat/python/compile.sh
@@ -1,1 +1,2 @@
-gcc -DLINUX -I.. -I/usr/lib/python2.5/site-packages/numpy/core/include/numpy/ -pedantic -Wall -std=c99 -g -O0 sift.c
+gcc -DLINUX -I.. -I/usr/lib/python2.5/site-packages/numpy/core/include/numpy/ -pedantic -Wall -std=c99 -g -O0 -c sift.c -o _sift.o
+gcc -lpython2.5 -shared _sift.o ../bin/glx/objs/*.o -o _sift.so
