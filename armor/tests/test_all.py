75187f7b12883659f4e473a5b07ec903c32090a7
wiecki <wiecki@d11de2a6-4b3f-0410-8375-bf80547001dc>
2008-05-12 14:23:13 +0000

    New, clean import

---
+++ armor/tests/test_all.py
@@ -0,0 +1,31 @@
+import unittest
+import armor
+import armor.tests
+import numpy
+
+class testAll(unittest.TestCase):
+    def setUp(self):
+	self.path = armor.tests.__path__[0]
+	self.imgDataset = armor.ImageDataset()
+	self.imgDataset.loadFromXML(self.path + '/test_valid.xml')
+
+
+    def testGenerator(self):
+	sft = armor.sift(self.imgDataset.getData(useGenerator=True))
+        km = armor.kmeans(sft.getData(useGenerator=True), 3)
+	data = list(km.getData())
+	self.assertEqual([x[1] for x in data], [u'test1', u'test2'])
+	#for x,y in zip(data, [numpy.array([475, 693, 531]), numpy.array([566, 782, 509])]):
+	#    self.assertTrue(all(x[0] == y))
+	
+    def testList(self):
+	sft = armor.sift(self.imgDataset.getData())
+	km = armor.kmeans(sft.getData(), 3)
+	data = list(km.getData())
+	self.assertEqual([x[1] for x in data], [u'test1', u'test2'])
+	#TODO: Call kmeans with fixed start vectors
+	#for x,y in zip(data, [numpy.array([475, 693, 531]), numpy.array([566, 782, 509])]):
+	#    self.assertTrue(all(x[0] == y))
+
+suite = unittest.TestLoader().loadTestsFromTestCase(testAll)
+unittest.TextTestRunner(verbosity=3).run(suite)
---
+++ setup.py
@@ -0,0 +1,55 @@
+import sys
+import os
+import platform
+
+scriptName = sys.argv[0]
+
+if not (scriptName == "setup.py" and os.path.exists("armor")):
+	print """
+Please start the installation from the directory above "armor" by executing
+"python setup.py build" 
+"python setup.py test"
+"python setup.py install"
+"""
+	exit(1)
+	
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, Extension
+try:
+    import numpy
+    import scipy
+    import PIL
+except:
+    print "You need to have numpy, scipy an PIL installed."
+    
+import glob
+
+siftModule = Extension('_sift',
+		       language = 'c++',
+		       sources = glob.glob('source/vlfeat/vl/*.c') + ['source/vlfeat/python/sift.c'],
+		       extra_compile_args=['-g', '-pedantic', '-Wall', '-std=c89', '-O0' ,'-Wno-unused-function', '-Wno-long-long', '-D__LITTLE_ENDIAN__', '-std=c99'],
+		       include_dirs = [numpy.get_include(), 'source/vlfeat'],
+		       extra_link_args = ['-lm'])
+
+kmeansModule = Extension('libmpikmeans',
+		       language = 'c++',
+		       sources = ['source/mpi_kmeans/mpi_kmeans.cxx'],
+		       extra_compile_args=['-Wl,-soname=libmpikmeans.so','-Wall', '-O3'])
+
+setup (name = 'Armor',
+       version = '0.1',
+       description = 'Object Recognition Toolkit for Orange',
+       author = 'Thomas V. Wiecki',
+       author_email = 'thomas.wiecki@gmail.com',
+       url = 'http://www.python.org/doc/current/ext/building.html',
+       long_description = '''...''',
+       ext_modules = [siftModule, kmeansModule],
+       packages = ['armor', 'armor.orngWidgets', 'armor.tests'],
+       include_package_data = True,
+       test_suite = "armor.tests.test_all",
+       zip_safe = False
+#       install_requires = ['numpy >= 1.0', 'scipy >= 0.5', 'PIL >= 1.1.6']
+       )
