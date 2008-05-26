664853f225afb10eecbf81b07d50890dac452e5a
wiecki <wiecki@d11de2a6-4b3f-0410-8375-bf80547001dc>
2008-05-26 20:40:39 +0000

    Divided into input- and outputSlots, TODO: extenisve unit tests

--- armor/slot.py
+++ armor/slot.py
@@ -1,11 +1,47 @@
 import armor
 from armor.SeqContainer import SeqContainer
 
-class slot(object):
-    def __init__(self, name, senderSlot=None, processFunc=None, processFuncs=None, group=None, inputType=None, outputType=None, slotType=None, iterator=None, sequence=None):
+class inputSlot(object):
+    def __init__(self, name, senderSlot=None, group=None, acceptsType=None):
         self.name = name
         self.senderSlot = senderSlot
-        self.inputType = inputType
+        self.group = group
+        self.acceptsType = acceptsType
+        self.container = None
+        self.converters = None
+	
+    def __iter__(self):
+        if not self.senderSlot:
+            raise KeyError, "No senderSlot registered!"
+        return iter(self.container)
+
+    def convertInput(self):
+        senderIterator = self.senderSlot.container.getIter(self.group)
+        for item in senderIterator:
+            for converter in self.converters:
+                item = converter(item)
+            yield item
+            
+    def registerInput(self, senderSlot):
+        if armor.useTypeChecking:
+            self.converters = self.acceptsType.compatible(senderSlot.outputType)
+            if self.converters == False:
+                raise TypeError, "Slots are not compatible"
+
+        self.senderSlot = senderSlot
+        self.container = SeqContainer(generator=self.convertInput, slot=self.senderSlot)
+        
+    def registerGroup(self, reference, group=None):
+        """Register to group."""
+        self.group = group
+        # Propagate further
+        self.senderSlot.register(group)
+
+        
+class outputSlot(object):
+    def __init__(self, name, input=None, processFunc=None, processFuncs=None, group=None, outputType=None, slotType=None, iterator=None, sequence=None):
+        self.name = name
+        self.inputSlot = input
         self.outputType = outputType
         self.group = group
         self.iterator = iterator
@@ -38,7 +74,7 @@ class slot(object):
         """Generator which iterates over the input elements, calling
         processFuncs and yields the processed element, one at a time.
         """
-        inputIter = self.senderSlot.container.getIter(group=self.group)
+        inputIter = self.inputSlot.container.getIter(group=self.group)
         for item in inputIter:
             for processFunc in self.processFuncs:
                 item = processFunc(item)
@@ -48,9 +84,9 @@ class slot(object):
 
     def bulkIterator(self):
         """Generator which iteratates over the input elements, saves them
-	and calls the processFuncs on the complete input data.
+        and calls the processFuncs on the complete input data.
         (e.g. clustering, normalization). """
-        inData = list(self.senderSlot.container.getIter(group=self.group))
+        inData = list(self.inputSlot.container.getIter(group=self.group))
 
         #self.senderSlot.container.reset()
         
@@ -60,20 +96,9 @@ class slot(object):
         for item in inData:
             yield item
             
-    def registerInput(self, senderSlot):
-        if armor.useTypeChecking:
-            converters = self.inputType.compatible(senderSlot.outputType)
-            if converters == False:
-                raise TypeError, "Slots are not compatible"
-            # Insert them at the beginning of the process cue
-            if len(converters) != 0:
-                converters.extend(self.processFuncs)
-                self.processFuncs = converters
-        self.senderSlot = senderSlot
-
     def registerGroup(self, reference, group=None):
         """Register to group."""
         self.group = group
         # Propagate further
-        self.senderSlot.register(group)
+        self.inputSlot.register(group)
     
--- armor/tests/test_slots.py
+++ armor/tests/test_slots.py
@@ -5,59 +5,66 @@ import armor.datatypes
 
 class TestTypes(unittest.TestCase):
     def setUp(self):
-	self.sequence = range(10)
+        self.sequence = range(10)
 
     def setSlotsNoConversion(self):
-	self.outType1 = armor.datatypes.ImageType(format='PIL', color_space='RGB')
-	self.inType2 = armor.datatypes.ImageType(format=['PIL'], color_space=['RGB'])
-	self.outType2 = armor.datatypes.VectorType(shape='nestedlist')
+        self.outType1 = armor.datatypes.ImageType(format='PIL', color_space='RGB')
+        self.inType2 = armor.datatypes.ImageType(format=['PIL'], color_space=['RGB'])
+        self.outType2 = armor.datatypes.VectorType(shape='nestedlist')
 
-	self.slotSend = armor.slot.slot('sender',
-				   iterator=self.iterator,
-				   outputType=self.outType1)
-				   
-	self.slotRecv = armor.slot.slot('receiver',
-				   processFunc=self.process,
-				   inputType=self.inType2,
-				   outputType=self.outType2)
+        self.slotSend = armor.slot.outputSlot('sender',
+                                              iterator=self.iterator,
+                                              outputType=self.outType1)
+
+        self.slotInput = armor.slot.inputSlot('input',
+                                              acceptsType=self.inType2)
+        
+        self.slotRecv = armor.slot.outputSlot('receiver',
+                                        input = self.slotInput,
+                                        processFunc=self.process,
+                                        outputType=self.outType2)
 
     def setSlotsConversion(self):
-	self.outType1 = armor.datatypes.ImageType(format='PIL', color_space='RGB')
-	self.inType2 = armor.datatypes.ImageType(format=['PIL'], color_space=['gray'])
-	self.outType2 = armor.datatypes.VectorType(shape='nestedlist')
+        self.outType1 = armor.datatypes.ImageType(format='PIL', color_space='RGB')
+        self.inType2 = armor.datatypes.ImageType(format=['PIL'], color_space=['gray'])
+        self.outType2 = armor.datatypes.VectorType(shape='nestedlist')
 
-	self.slotSend = armor.slot.slot('sender',
-				   iterator=self.iterator,
-				   outputType=self.outType1)
-				   
-	self.slotRecv = armor.slot.slot('receiver',
-				   processFunc=self.process,
-				   inputType=self.inType2,
-				   outputType=self.outType2)
-	
+        self.slotSend = armor.slot.outputSlot('sender',
+                                              iterator=self.iterator,
+                                              outputType=self.outType1)
+
+        self.slotInput = armor.slot.inputSlot('input',
+                                              acceptsType=self.inType2)
+        
+        self.slotRecv = armor.slot.outputSlot('receiver',
+                                        input = self.slotInput,
+                                        processFunc=self.process,
+                                        outputType=self.outType2)
+        
     def testSlotConnectNoConversion(self):
-	self.setSlotsNoConversion()
-	self.slotRecv.registerInput(self.slotSend)
-	self.assertEqual([i.__name__ for i in self.slotRecv.processFuncs], ['process'])
-	
+        self.setSlotsNoConversion()
+        self.slotInput.registerInput(self.slotSend)
+        self.assertEqual([i.__name__ for i in self.slotRecv.processFuncs], ['process'])
+        
     def testSlotConnectConversion(self):
-	self.setSlotsConversion()
-	self.slotRecv.registerInput(self.slotSend)
-	self.assertEqual([i.__name__ for i in self.slotRecv.processFuncs], ['convert_RGB_to_gray', 'process'])
+        self.setSlotsConversion()
+        self.slotInput.registerInput(self.slotSend)
+	self.assertEqual([i.__name__ for i in self.slotInput.converters], ['convert_RGB_to_gray'])
+        self.assertEqual([i.__name__ for i in self.slotRecv.processFuncs], ['process'])
 
     def testIterating(self):
-	self.setSlotsNoConversion()
-	self.slotRecv.registerInput(self.slotSend)
+        self.setSlotsNoConversion()
+        self.slotInput.registerInput(self.slotSend)
+        self.assertEqual([i for i in self.slotRecv], range(10))
 	self.assertEqual([i for i in self.slotRecv], range(10))
 
-
-	
+        
     def process(self, item):
-	return item
+        return item
 
     def iterator(self):
-	for i in xrange(10):
-	    yield i
+        for i in xrange(10):
+            yield i
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestTypes)
 unittest.TextTestRunner(verbosity=3).run(suite)
