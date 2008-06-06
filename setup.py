cc2862fab3ddad83c5657885f242b1b1bedadddb
wiecki <wiecki@d11de2a6-4b3f-0410-8375-bf80547001dc>
2008-06-06 12:07:08 +0000

    Moved mpi_kmeans and vlfeat into src

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ import glob
 
 siftModule = Extension('_sift',
 		       language = 'c++',
-		       sources = glob.glob('source/vlfeat/vl/*.c') + ['source/vlfeat/python/sift.c'],
+		       sources = glob.glob('vlfeat/vl/*.c') + ['vlfeat/python/sift.c'],
 		       extra_compile_args=['-g', '-pedantic', '-Wall', '-std=c89', '-O3' ,'-Wno-unused-function', '-Wno-long-long', '-D__LITTLE_ENDIAN__', '-std=c99'],
-		       include_dirs = [numpy.get_include(), 'source/vlfeat'],
+		       include_dirs = [numpy.get_include(), 'vlfeat'],
 		       extra_link_args = ['-lm'])
 
 kmeansModule = Extension('libmpikmeans',
 		       language = 'c++',
-		       sources = ['source/mpi_kmeans/mpi_kmeans.cxx'],
+		       sources = ['mpi_kmeans/mpi_kmeans.cxx'],
 		       extra_compile_args=['-Wl,-soname=libmpikmeans.so','-Wall', '-O3'])
 
 
